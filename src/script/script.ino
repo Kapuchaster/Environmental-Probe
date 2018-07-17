dfbcf88ce0612d68516191e4568f59aa87e7525a
Michal Kapuscinski <kapuchaster@gmail.com>
2018-07-17 20:26:32 +0200

    Init MCP sensor

--- src/script/script.ino
+++ src/script/script.ino
@@ -5,6 +5,7 @@
 #include "Adafruit_SHT31.h"
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP280.h>
+#include "Adafruit_MCP9808.h"
 
 #define BMP_SCK 13
 #define BMP_MISO 12
@@ -13,6 +14,7 @@
 
 Adafruit_SHT31 sht31 = Adafruit_SHT31();
 Adafruit_BMP280 bmp; // I2C
+Adafruit_MCP9808 tempsensor = Adafruit_MCP9808();
 
 void setup() {
   Serial.begin(9600);
@@ -22,6 +24,8 @@ void setup() {
 
   Serial.println("SHT31 test");
   Serial.println(F("BMP280 test"));
+    Serial.println("MCP9808 demo");
+
 
   if (!sht31.begin(0x44)) {   // Set to 0x45 for alternate i2c addr
     Serial.println("Couldn't find SHT31");
@@ -32,9 +36,26 @@ void setup() {
     Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
     while (1);
   }
+
+  if (!tempsensor.begin()) {
+    Serial.println("Couldn't find MCP9808!");
+    while (1);
+  }
 }
 
 void loop() {
+  Serial.println("SHT");
+  SHT();
+  Serial.println("BMP");
+  BMP();
+  Serial.println("MCP");
+  MCP();
+  
+  Serial.println("------------------------------");
+  delay(1000);
+}
+
+void SHT() {
   float t = sht31.readTemperature();
   float h = sht31.readHumidity();
 
@@ -49,19 +70,26 @@ void loop() {
   } else { 
     Serial.println("Failed to read humidity");
   }
-
-    Serial.print(F("Temperature = "));
-    Serial.print(bmp.readTemperature());
-    Serial.println(" *C");
-    
-    Serial.print(F("Pressure = "));
-    Serial.print(bmp.readPressure());
-    Serial.println(" Pa");
-
-    Serial.print(F("Approx altitude = "));
-    Serial.print(bmp.readAltitude(1013.25)); // this should be adjusted to your local forcase
-    Serial.println(" m");
-    
-  Serial.println();
-  delay(1000);
 }
+
+void MCP() {
+  float c = tempsensor.readTempC();
+  float f = c * 9.0 / 5.0 + 32;
+  Serial.print("Temp: "); Serial.print(c); Serial.print("*C\t"); 
+  Serial.print(f); Serial.println("*F");
+}
+
+void BMP() {
+  Serial.print(F("Temperature = "));
+  Serial.print(bmp.readTemperature());
+  Serial.println(" *C");
+  
+  Serial.print(F("Pressure = "));
+  Serial.print(bmp.readPressure());
+  Serial.println(" Pa");
+
+  Serial.print(F("Approx altitude = "));
+  Serial.print(bmp.readAltitude(1013.25)); // this should be adjusted to your local forcase
+  Serial.println(" m");
+}
+
