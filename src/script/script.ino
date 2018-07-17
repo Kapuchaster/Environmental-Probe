3ea99303f987e5bc714d5bd2ed12e89d508b2bf3
Michal Kapuscinski <kapuchaster@gmail.com>
2018-07-17 20:11:53 +0200

    Init BMP sensor

--- src/script/script.ino
+++ src/script/script.ino
@@ -1,8 +1,18 @@
 #include <Arduino.h>
 #include <Wire.h>
+#include <SPI.h>
+
 #include "Adafruit_SHT31.h"
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BMP280.h>
+
+#define BMP_SCK 13
+#define BMP_MISO 12
+#define BMP_MOSI 11 
+#define BMP_CS 10
 
 Adafruit_SHT31 sht31 = Adafruit_SHT31();
+Adafruit_BMP280 bmp; // I2C
 
 void setup() {
   Serial.begin(9600);
@@ -11,9 +21,16 @@ void setup() {
     delay(10);     // will pause Zero, Leonardo, etc until serial console opens
 
   Serial.println("SHT31 test");
-  if (! sht31.begin(0x44)) {   // Set to 0x45 for alternate i2c addr
+  Serial.println(F("BMP280 test"));
+
+  if (!sht31.begin(0x44)) {   // Set to 0x45 for alternate i2c addr
     Serial.println("Couldn't find SHT31");
     while (1) delay(1);
+  }
+
+  if (!bmp.begin()) {  
+    Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
+    while (1);
   }
 }
 
@@ -32,6 +49,19 @@ void loop() {
   } else { 
     Serial.println("Failed to read humidity");
   }
+
+    Serial.print(F("Temperature = "));
+    Serial.print(bmp.readTemperature());
+    Serial.println(" *C");
+    
+    Serial.print(F("Pressure = "));
+    Serial.print(bmp.readPressure());
+    Serial.println(" Pa");
+
+    Serial.print(F("Approx altitude = "));
+    Serial.print(bmp.readAltitude(1013.25)); // this should be adjusted to your local forcase
+    Serial.println(" m");
+    
   Serial.println();
   delay(1000);
 }
